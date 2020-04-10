eb819c1d348bb977a4efe786a3b764b8af2ee707
ashrafSayed <ashrafsayed19841111@gmail.com>
2020-04-10 20:08:57 +0300

    lesson 18

--- dist/js/main.js
+++ dist/js/main.js
@@ -10,4 +10,46 @@ $(function () {
        $(this).find('.fa-angle-right').toggleClass('down');
        $(this).next('.child-linls').slideToggle();
     })
+
+    // open & close the full screen 
+
+    $('.toggle-fullscreen').on('click',function () {
+        $(this).toggleClass('full-screen');
+        if($(this).hasClass('full-screen')) {  // page is now full screen 
+            openFullscreen();
+        } else { // page is not full screen 
+            closeFullscreen();
+        }
+     })
+
+     /* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+    /* View in fullscreen */
+    function openFullscreen() {
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.mozRequestFullScreen) { /* Firefox */
+            elem.mozRequestFullScreen();
+        } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE/Edge */
+            elem.msRequestFullscreen();
+        }
+    }
+
+    /* Close fullscreen */
+    function closeFullscreen() {
+
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) { /* Firefox */
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { /* IE/Edge */
+            document.msExitFullscreen();
+        }
+    }
+
 })
