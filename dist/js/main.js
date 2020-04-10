3c757140ab13b3a2564c5a71cee07dbb40b0c029
ashrafSayed <ashrafsayed19841111@gmail.com>
2020-04-10 03:27:49 +0300

    lesson 12,13.14

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,5 +2,12 @@ $(function () {
     "use strict";
     $('.toggle-sidebar').on("click",function () {
         $(".content-area,.sidebar").toggleClass('no-sidebar');
+    });
+
+    // Toggle Submenu 
+
+    $('.toggle-submenu').on('click',function () {
+       $(this).find('.fa-angle-right').toggleClass('down');
+       $(this).next('.child-linls').slideToggle();
     })
 })
